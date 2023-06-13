56ece03a639d835641c13f115b4b07cb7c9b9992
Diana Isaeva <96681256+diink-iis@users.noreply.github.com>
2023-06-13 09:05:26 +0300

    Update parser_hh.py

--- parser_hh.py
+++ parser_hh.py
@@ -1,14 +1,21 @@
 import requests
 import pandas as pd
 
+# количество страниц для выгрузки
 number_of_pages = 100
 
+# заголовки вакансий для парсинга
 job_title = ["'Data Analyst' and 'data scientist'"]
+
+# проходимся по каждой вакансии
 for job in job_title:
     data = []
+    # проходимся по каждой странице
     for i in range(number_of_pages):
+        # API для парсинга
         url = 'https://api.hh.ru/vacancies'
         par = {'text': job, 'area': '113', 'per_page': '10', 'page': i}
+        # с помощью бибилотеки выгружаем информацию с HTML кода страницы и сохраняем все в json строки
         r = requests.get(url, params=par)
         e = r.json()
         data.append(e)
@@ -19,5 +26,6 @@ for job in job_title:
             for j in range(len(data[k]['items'])):
                 df.loc[ind] = data[k]['items'][j]
                 ind += 1
+    # выгрузка в csv
     csv_name = job + ".csv"
     df.to_csv(csv_name)
